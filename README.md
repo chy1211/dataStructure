0e281382766dcfefc88db821aa084214dbec1e90
chy1211 <henry70304@gmail.com>
2023-01-27 02:02:26 +0800

    week8

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # dataStructure
 
 ## week5
-### https://www3.ntu.edu.sg/home/ehchua/programming/java/J3b_OOPInheritancePolymorphism.html
+##### https://www3.ntu.edu.sg/home/ehchua/programming/java/J3b_OOPInheritancePolymorphism.html
 ## week6
-### ![image](https://cdn.discordapp.com/attachments/1022149378942717972/1032189681078648913/IMG_0075.jpg)
+##### ![image](https://cdn.discordapp.com/attachments/1022149378942717972/1032189681078648913/IMG_0075.jpg)
 ## midTerm
-### 70題練習
+##### 70題練習
+## week8
+##### DoublyLinkedList
--- src/week8/DoublyLinkedList.java
+++ src/week8/DoublyLinkedList.java
@@ -9,13 +9,80 @@ package week8;
  *
  * @author henry
  */
-public class DoublyLinkedList {
+class Node{
+    int value;
+    Node next;
+    Node prev;
+    public Node(int value){
+        this.value = value;
+        this.next = null;
+        this.prev = null;
+    }
+}
+class dl_list {
+    Node head;
+    Node tail;
+    public dl_list(){
+        this.head = null;
+        this.tail = null;
+    }
 
+    public void add(int value){
+        Node newNode = new Node(value);
+        if(this.head == null){
+            this.head = newNode;
+            this.tail = newNode;
+        }else{
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode;
+        }
+    }
+    public void addLast(int value) {
+        Node newNode = new Node(value);
+        if (tail == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.prev = tail;
+            tail.next = newNode;
+            tail = newNode;
+        }
+    }
+
+    public void removeFirst() {
+        if (head != null) {
+            head = head.next;
+            head.prev = null;
+        }
+    }
+
+    public void removeLast() {
+        if (tail != null) {
+            tail = tail.prev;
+            tail.next = null;
+        }
+    }
+
+    public void printList() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.value + " ");
+            current = current.next;
+        }
+    }
+}
+public class doublyLinkedList {
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("HelloWorld!");
+        dl_list dl_list = new dl_list();
+        dl_list.add(1);
+        dl_list.add(2);
+        dl_list.add(3);
+        dl_list.add(4);
+        dl_list.add(5);
+        dl_list.printList();
     }
-    
 }
